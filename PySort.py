2b4c98d913a1a3a6f616fefa70777af6dd8c1048
Joseph B <satdarn@gmail.com>
2022-11-13 14:57:19 -0800

    Added QuickSort

--- PySort.py
+++ PySort.py
@@ -3,24 +3,16 @@ class insertSort():
         self.unsorted = sortList
         self.sorted = []
         self.reverseSorted = []
-        self.snapshots = {}
-    def returnUnsorted(self, printUnsorted = False):
-        if printUnsorted:
-            print(self.unsorted)
-        return self.unsorted
-    def returnSorted(self, printSorted = False):
+        #forwards sort (least to greatest)
         arr = self.unsorted
         for i in range(1, len(arr)):
             j = i
-
             while arr[j-1] > arr[j] and j > 0:
                 arr[j-1], arr[j] = arr[j], arr[j-1]
                 j -= 1        
         self.sorted = arr
-        if printSorted:
-            print(self.sorted)
-        return self.sorted
-    def returnReverseSorted(self, printSorted = False):
+
+        #backwards or reverse sort (greatest to least)
         arr = self.unsorted
         for i in range(1, len(arr)):
             j = i
@@ -28,6 +20,21 @@ class insertSort():
                 arr[j-1], arr[j] = arr[j], arr[j-1] 
                 j -= 1  
         self.reverseSorted = arr
+
+
+    def returnUnsorted(self, printUnsorted = False):
+        if printUnsorted:
+            print(self.unsorted)
+        return self.unsorted
+
+
+    def returnSorted(self, printSorted = False):
+        if printSorted:
+            print(self.sorted)
+        return self.sorted
+
+
+    def returnReverseSorted(self, printSorted = False):
         if printSorted:
             print(self.reverseSorted)
         return self.reverseSorted
@@ -37,12 +44,7 @@ class selectionSort():
         self.unsorted = sortList
         self.sorted = []
         self.reverseSorted = []
-        self.snapshots = {}
-    def returnUnsorted(self, printUnsorted = False):
-        if printUnsorted:
-            print(self.unsorted)
-        return self.unsorted
-    def returnSorted(self, printSorted = False):
+        #forwards sort (least to greatest)
         arr = self.unsorted
         for i in range(0, len(arr)-1):
             curMinIndex = i
@@ -51,23 +53,91 @@ class selectionSort():
                     curMinIndex = j
             arr[i], arr[curMinIndex] = arr[curMinIndex], arr[i]
         self.sorted = arr
-        if printSorted:
-            print(self.sorted)
-        return self.sorted
-    def returnReverseSorted(self, printSorted = False):
+
+        #backwards or reverse sort (greatest to least)
         arr = self.unsorted
         for i in range(0, len(arr)-1):
             curMaxIndex = i
             for j in range(i+1, len(arr)):
-                if arr[j] < arr[curMaxIndex]:
+                if arr[j] > arr[curMaxIndex]:
                     curMaxIndex = j
             arr[i], arr[curMaxIndex] = arr[curMaxIndex], arr[i]
         self.reverseSorted = arr
+
+
+    def returnUnsorted(self, printUnsorted = False):
+        if printUnsorted:
+            print(self.unsorted)
+        return self.unsorted
+
+
+    def returnSorted(self, printSorted = False):
+        if printSorted:
+            print(self.sorted)
+        return self.sorted
+
+
+    def returnReverseSorted(self, printSorted = False):
         if printSorted:
             print(self.reverseSorted)
         return self.reverseSorted
 
+class quickSort():
+    def __init__(self, sortList = []):
+        self.unsorted = sortList
+        self.sorted = []
+        self.reverseSorted = []
+        
+        def forwardsRecursive(arr, left, right):
+            if left < right:
+                partition_pos = partition(arr, left, right)
+                forwardsRecursive(arr, left, partition_pos -1)
+                forwardsRecursive(arr, partition_pos+1, right)
+            return arr
+        def backwardsRecursive(arr):
+            reverseArr = []
+            for i in range(len(arr)-1, 0):
+                reverseArr.append(arr[i])
+            return reverseArr
 
+
+            
+        
+        def partition(arr, left, right):
+            i = left
+            j = right-1
+            pivot =  arr[right]
+            while i < j:
+                while i < right and arr[i] < pivot:
+                    i += 1
+                while j > left and arr[j] > pivot:
+                    j -= 1
+                if i < j:
+                    arr[j], arr[i] = arr[i], arr[j]
+            if arr[i] > pivot:
+                arr[i], arr[right] = arr[right], arr[i]
+                return i 
+        #forwards sort (least to greatest)
+        arr = self.unsorted
+        self.sorted = forwardsRecursive(arr, 0, len(arr)-1)
+        #backwards or reverse sort (greatest to least)
+        arr = self.unsorted
+
+        self.reverseSorted = backwardsRecursive(arr)
+    def returnUnsorted(self, printUnsorted = False):
+        if printUnsorted:
+            print(self.unsorted)
+        return self.unsorted
+    def returnSorted(self, printSorted = False):
+        arr = self.unsorted
+
+        if printSorted:
+            print(self.sorted)
+        return self.sorted
+    def returnReverseSorted(self, printSorted = False):
+        if printSorted:
+            print(self.reverseSorted)
+        return self.reverseSorted
 
 
 
@@ -78,15 +148,26 @@ class templateAlgo():
         self.unsorted = sortList
         self.sorted = []
         self.reverseSorted = []
-        self.snapshots = {}
+        #forwards sort (least to greatest)
+        arr = self.unsorted
+
+        self.sorted = arr
+        #backwards or reverse sort (greatest to least)
+        arr = self.unsorted
+
+        self.reverseSorted = arr
     def returnUnsorted(self, printUnsorted = False):
         if printUnsorted:
             print(self.unsorted)
         return self.unsorted
+
+
     def returnSorted(self, printSorted = False):
         if printSorted:
             print(self.sorted)
         return self.sorted
+
+
     def returnReverseSorted(self, printSorted = False):
         if printSorted:
             print(self.reverseSorted)
