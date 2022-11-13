10ec24c31f26e482b972a689c2e704d05dfd02a6
Joseph B <satdarn@gmail.com>
2022-11-13 13:46:28 -0800

    snapshots removed

--- PySort.py
+++ PySort.py
@@ -8,26 +8,22 @@ class insertSort():
         if printUnsorted:
             print(self.unsorted)
         return self.unsorted
-    def returnSorted(self, printSorted = False, snapshots = False):
+    def returnSorted(self, printSorted = False):
         arr = self.unsorted
         for i in range(1, len(arr)):
             j = i
-            if snapshots:
-                    self.snapshots[f"Step {j}"] = arr  
+
             while arr[j-1] > arr[j] and j > 0:
                 arr[j-1], arr[j] = arr[j], arr[j-1]
-                j -= 1
-                 
+                j -= 1        
         self.sorted = arr
         if printSorted:
             print(self.sorted)
         return self.sorted
-    def returnReverseSorted(self, printSorted = False, snapshots = False):
+    def returnReverseSorted(self, printSorted = False):
         arr = self.unsorted
         for i in range(1, len(arr)):
             j = i
-            if snapshots:
-                self.snapshots[f"Reverse Step {i}"] = arr
             while arr[j-1] < arr[j] and j > 0:
                 arr[j-1], arr[j] = arr[j], arr[j-1] 
                 j -= 1  
@@ -46,11 +42,9 @@ class selectionSort():
         if printUnsorted:
             print(self.unsorted)
         return self.unsorted
-    def returnSorted(self, printSorted = False, snapshots = False):
+    def returnSorted(self, printSorted = False):
         arr = self.unsorted
         for i in range(0, len(arr)-1):
-            if snapshots:
-                self.snapshots[f"Step {i+1}"] = arr
             curMinIndex = i
             for j in range(i+1, len(arr)):
                 if arr[j] < arr[curMinIndex]:
@@ -60,11 +54,9 @@ class selectionSort():
         if printSorted:
             print(self.sorted)
         return self.sorted
-    def returnReverseSorted(self, printSorted = False, snapshots = False):
+    def returnReverseSorted(self, printSorted = False):
         arr = self.unsorted
         for i in range(0, len(arr)-1):
-            if snapshots:
-                self.snapshots[f"ReverseStep {i+1}"] = arr
             curMaxIndex = i
             for j in range(i+1, len(arr)):
                 if arr[j] < arr[curMaxIndex]:
@@ -91,11 +83,11 @@ class templateAlgo():
         if printUnsorted:
             print(self.unsorted)
         return self.unsorted
-    def returnSorted(self, printSorted = False, snapshots = False):
+    def returnSorted(self, printSorted = False):
         if printSorted:
             print(self.sorted)
         return self.sorted
-    def returnReverseSorted(self, printSorted = False, snapshots = False):
+    def returnReverseSorted(self, printSorted = False):
         if printSorted:
             print(self.reverseSorted)
         return self.reverseSorted
